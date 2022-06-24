ebb093f5ef6f78323c39706e24e6ef7e439ce912
Lucas Corrêa <89353709+LaKHamote@users.noreply.github.com>
2022-06-24 11:13:17 -0300

    Add files via upload

---
+++ dominios.hpp
@@ -0,0 +1,14 @@
+// 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
+
+using namespace std;
+
+class Name {
+    private:
+        string name;
+    public:
+        void setName(string);
+        string getName() const;
+};
+inline Name::getName() const {
+    return name
+}
