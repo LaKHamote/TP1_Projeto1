540cbf974974437d64e4153cbabb2494066ae8e7
LucasDem014 <101001160+LucasDem014@users.noreply.github.com>
2022-08-21 04:04:13 -0300

    Pasta trabalho_2 MAC e MAA concluídos

---
+++ Trabalho_2/entity.hpp
@@ -0,0 +1,116 @@
+#ifndef entityHppIncluded
+#define entityHppIncluded
+
+#include "domain.hpp"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <unordered_set>
+
+using namespace std;
+
+/**
+ * Portrayal of User that is composed of 6 classes:
+ *
+ * -Name
+ *
+ * -Email
+ *
+ * -Password
+ *
+ * -Language
+ *
+ * -Date
+ *
+ * -Description
+ *
+ * Author: 211038262
+ */
+class User {
+    private:
+        Name name;
+        Email email;
+        Password password;
+        Language language;
+        Date date;
+        Description description;
+    public:
+        Name getName() const { return name; };
+        Email getEmail() const { return email; };
+        Password getPassword() const { return password; };
+        Language getLanguage() const { return language; };
+        Date getDate() const { return date; };
+        Description getDescription() const { return description; };
+        void setName(const Name& name) { this->name = name; };
+        void setEmail(const Email& email) { this->email = email; };
+        void setPassword(const Password& password) { this->password = password; };
+        void setLanguage(const Language& language) { this->language = language; };
+        void setDate(const Date& date) { this->date = date; };
+        void setDescription(const Description& description) { this->description = description; };
+};
+
+/**
+ * Portrayal of an Accommodation that is composed of 6 classes:
+ *
+ * -Code
+ *
+ * -City
+ *
+ * -Country
+ *
+ * -Grade
+ *
+ * -Description
+ *
+ * Author: 211038262
+ */
+class Accommodation {
+    private:
+        Code code;
+        City city;
+        Country country;
+        Grade grade;
+        Description description;
+    public:
+        Code getCode() const { return code; };
+        City getCity() const { return city; };
+        Country getCountry() const { return country; };
+        Grade getGrade() const { return grade; };
+        Description getDescription() const { return description; };
+        void setCode(const Code& code) { this->code = code; };
+        void setCity(const City& city) { this->city = city; };
+        void setCountry(const Country& country) { this->country = country; };
+        void setGrade(const Grade& grade) { this->grade = grade; };
+        void setDescription(const Description& description) { this->description = description; };
+};
+
+/**
+ * Portrayal of an Accommodation that is composed of 6 classes:
+ *
+ * -Code
+ *
+ * -City
+ *
+ * -Country
+ *
+ * -Grade
+ *
+ * -Description
+ *
+ * Author: 211026673
+ */
+class Rating {
+    private:
+        Code code;
+        Grade grade;
+        Description description;
+    public:
+        Code getCode() const { return code; };
+        Grade getGrade() const { return grade; };
+        Description getDescription() const { return description; };
+        void setCode(const Code& code) { this->code = code; };
+        void setGrade(const Grade& grade) { this->grade = grade; };
+        void setDescription(const Description& description) { this->description = description; };
+};
+
+#endif
